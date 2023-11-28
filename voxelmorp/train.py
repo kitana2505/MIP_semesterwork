d55bfa268f3ef6d1f69b794174bedc23c25402cb
Anh Quang Thi Minh <quangthi@cmpgrid-79.felk.cvut.cz>
2023-11-28 18:05:28 +0100

    Update batch training

--- voxelmorp/train.py
+++ voxelmorp/train.py
@@ -11,8 +11,11 @@ import neurite as ne
 from keras.callbacks import ModelCheckpoint
 from helper import load_data, train_test_split
 import datetime
+import ipdb
 
-def vxm_data_generator(x_data, batch_size=32):
+RESIZE_HEIGHT = 128
+
+def vxm_data_generator(data_paths, resize, batch_size=32):
     """
     Generator that takes in data of size [N, H, W], and yields data for
     our custom vxm model. Note that we need to provide numpy data for each
@@ -22,21 +25,24 @@ def vxm_data_generator(x_data, batch_size=32):
     outputs: moved image [bs, H, W, 1], zero-gradient [bs, H, W, 2]
     """
 
+    data_paths = np.array(data_paths)
+
     # preliminary sizing
-    vol_shape = x_data.shape[1:] # extract data shape
-    ndims = len(vol_shape)
+    ndims = len(resize)
 
     # prepare a zero array the size of the deformation
     # we'll explain this below
-    zero_phi = np.zeros([batch_size, *vol_shape, ndims])
+    zero_phi = np.zeros([batch_size, resize[1], resize[0], ndims])
 
     while True:
         # prepare inputs:
         # images need to be of the size [batch_size, H, W, 1]
         idx1 = np.random.randint(0, x_data.shape[0], size=batch_size)
-        moving_images = x_data[idx1, ..., np.newaxis]
+
+        moving_images = load_data(data_paths[idx1], resize)[:,:,:, np.newaxis]
+
         idx2 = np.random.randint(0, x_data.shape[0], size=batch_size)
-        fixed_images = x_data[idx2, ..., np.newaxis]
+        fixed_images = load_data(data_paths[idx2], resize)[:,:,:, np.newaxis]
         inputs = [moving_images, fixed_images]
 
         # prepare outputs (the 'true' moved image):
@@ -44,18 +50,24 @@ def vxm_data_generator(x_data, batch_size=32):
         # the resulting moved image with the fixed image.
         # we also wish to penalize the deformation field.
         outputs = [fixed_images, zero_phi]
-
         yield (inputs, outputs)
 
 if __name__=="__main__":
     data_root = "/home.stud/quangthi/ws/data/Sada_imgs"
 
-    train_data = load_data(data_root)
-    x_train, x_val = train_test_split(train_data, val_per=0.1)
+    # train_data = load_data(data_root)
+    # x_train, x_val = train_test_split(train_data, val_per=0.1)
     
-    # build model using VxmDense
-    inshape = x_train.shape[1:]
+    data_paths = glob(os.path.join(data_root, "*/*.png"))   
+    x_data = cv2.imread(data_paths[0], cv2.IMREAD_GRAYSCALE)
 
+    height, width = x_data.shape[:]
+    # resize by the ratio of heigh / width
+    resize_width = int(RESIZE_HEIGHT * width / height / 10) * 10
+
+    # build model using VxmDense
+    inshape = (RESIZE_HEIGHT, resize_width)
+    new_size = (resize_width, RESIZE_HEIGHT)
     # configure unet features
     nb_features = [
         [32, 32, 32, 32],         # encoder features
@@ -75,8 +87,8 @@ if __name__=="__main__":
 
     vxm_model.compile(optimizer='Adam', loss=losses, loss_weights=loss_weights)
 
-    batch_size = 16
-    train_generator = vxm_data_generator(x_train, batch_size)
+    batch_size = 64
+    train_generator = vxm_data_generator(data_paths, resize=new_size, batch_size=batch_size)
 
     checkpoint = ModelCheckpoint(
         'voxel_morp.h5',  # Filepath to save the model
@@ -93,10 +105,10 @@ if __name__=="__main__":
     callbacks_list = [checkpoint, tensorboard_callback]
 
     nb_epochs = 100
-    steps_per_epoch = len(x_train) // batch_size
+    steps_per_epoch = len(data_paths) // batch_size
 
     print("[INFO] Start training....")
     hist = vxm_model.fit(train_generator, 
     epochs=nb_epochs, steps_per_epoch=steps_per_epoch, 
     callbacks=callbacks_list,
-    verbose=2)
+    verbose=1)
